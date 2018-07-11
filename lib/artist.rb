a6deffecd238e76cbc28722225bfa24ec48413f7
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 07:43:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-  include Memoreable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
